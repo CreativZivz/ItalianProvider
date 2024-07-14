8726cfd47694a8ceff7e10066db47168b7dd9ce3
CreativZiv <168580072+CreativZivz@users.noreply.github.com>
2024-07-14 19:17:58 +0200

    Update StreamingcommunityProvider.kt

--- StreamingcommunityProvider/src/main/kotlin/com/lagradost/StreamingcommunityProvider.kt
+++ StreamingcommunityProvider/src/main/kotlin/com/lagradost/StreamingcommunityProvider.kt
@@ -11,7 +11,7 @@ import java.security.MessageDigest
 
 class StreamingcommunityProvider : MainAPI() {
     override var lang = "it"
-    override var mainUrl = "https://streamingcommunity.expert"
+    override var mainUrl = "https://streamingcommunity.boston"
     override var name = "StreamingCommunity"
     override val hasMainPage = true
     override val hasChromecastSupport = true
